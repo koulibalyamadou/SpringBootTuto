0f14b74128eca395b58afb5fe49df0c257da66b2
koulibaly <koulibalyamadou@outlook.com>
2024-07-03 10:52:49 +0000

    first commit

---
+++ src/main/java/gn/kinnovate/koulibaly/SpringBootTutoApplication.java
@@ -0,0 +1,13 @@
+package gn.kinnovate.koulibaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gn/kinnovate/koulibaly/SpringBootTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.kinnovate.koulibaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
